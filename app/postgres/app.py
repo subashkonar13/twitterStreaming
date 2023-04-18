27a6796f17c3377c14968ac2745be259e128ab03
subashkonar13 <subashkonar13@gmail.com>
2023-04-19 01:48:56 +0530

    Update app.py

--- app/postgres/app.py
+++ app/postgres/app.py
@@ -1,55 +1,74 @@
 import time
 import random
+#from sqlalchemy import create_engine,text
+#import pyspark
 import psycopg2
 import json
 
 
-with open('conf/credentials.json','r') as f:
-    credentials = json.load(f)
-DBUSER: str = credentials['db_user']
-DBPASS: str = credentials['db_pass']
-DBHOST: str = credentials['db_host']
-DBPORT: str = credentials['db_port']
-DBNAME: str = credentials['db_name']
+def connect_db() -> tuple:
+    """
+    Connects to the database using the credentials in the "conf/credentials.json" file.
 
-    
-def connectdb():
-   try:
-       connection = psycopg2.connect(user=DBUSER,
-                                  password=DBPASS,
-                                  host=DBHOST,
-                                  port=DBPORT,
-                                  database=DBNAME)
-       cursor = connection.cursor()
-       return cursor,connection
-   except (Exception, psycopg2.Error) as error:
-       print("Failed to establish Connection", error)
-    
-    
-#def writePyscopg(i,text,created_at):
-def writeTweets(*params):
+    Returns:
+        A tuple containing the cursor and connection objects.
+    """
     try:
-       cursor,connection=connectdb()
-       tweet_query = """ INSERT INTO Tweet_Data (user_id,tweet_id, tweet_text,sentiment,comp,Emoticon,created_at) VALUES (%s,%s,%s,%s,%s,%s,%s)"""
-       tweet_to_insert = (params)
-       cursor.execute(tweet_query, tweet_to_insert)
-       connection.commit()
-       #count = cursor.rowcount
-       print("Record inserted successfully into table")
-
+        with open('conf/credentials.json', 'r') as f:
+            credentials = json.load(f)
+        db_user:str = credentials['db_user']
+        db_pass:str = credentials['db_pass']
+        db_host:str = credentials['db_host']
+        db_port:str = credentials['db_port']
+        db_name:str = credentials['db_name']
+        connection = psycopg2.connect(
+            user=db_user,
+            password=db_pass,
+            host=db_host,
+            port=db_port,
+            database=db_name
+        )
+        cursor = connection.cursor()
+        return cursor, connection
     except (Exception, psycopg2.Error) as error:
-       print("Failed to insert record into Tweet_Data table", error)
+        print("Failed to establish connection", error)
+
+def write_tweets(*params: tuple) -> None:
+    """
+    Inserts a new record into the "Tweet_Data" table.
+
+    Args:
+        *params: A tuple containing the values to be inserted into the table.
+
+    Returns:
+        None
+    """
+    try:
+        cursor, connection = connect_db()
+        tweet_query = """INSERT INTO Tweet_Data (user_id, tweet_id, tweet_text, sentiment, comp, Emoticon, created_at)
+                          VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+        cursor.execute(tweet_query, params)
+        connection.commit()
+        print("Record inserted successfully into table")
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to insert record into Tweet_Data table", error)
 
        
-def writeTrends(*p):
-    try:
-       cursor,connection=connectdb()
-       trending_query = """ INSERT INTO Trending_Data (trend_name, url, tweet_volume) VALUES (%s,%s,%s)"""
-       trend_to_insert = (p)
-       cursor.execute(trending_query, trend_to_insert)
-       connection.commit()
-       #count = cursor.rowcount
-       print("Record inserted successfully into table")
+def write_trends(*p: tuple) -> None:
+    """
+    Insert trending data into the Trending_Data table.
 
+    Args:
+    *params: tuple: A tuple of trending data to be inserted.
+
+    Returns:
+    None
+    """
+    try:
+        cursor, connection = connect_db()
+        trending_query = """ INSERT INTO Trending_Data (trend_name, url, tweet_volume) VALUES (%s, %s, %s)"""
+        cursor.execute(trending_query, p)
+        connection.commit()
+        print("Record inserted successfully into table")
     except (Exception, psycopg2.Error) as error:
-       print("Failed to insert record into Trending_Data table", error)      
+        print("Failed to insert record into Trending_Data table", error)    
