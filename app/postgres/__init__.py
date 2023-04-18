f910bb5aa4f6fa3eac8a20d12ce0cdb2bf9ec793
subashkonar13 <subashkonar13@gmail.com>
2023-04-19 01:49:59 +0530

    Update __init__.py

--- app/postgres/__init__.py
+++ app/postgres/__init__.py
@@ -1,5 +1,2 @@
-#from .app import insert_tweet_data
-#from .app import writetoPostgres
-from .app import writeTweets
-#from .app import writeEmoticons
-from .app import writeTrends
+from .app import write_tweets
+from .app import write_trends
