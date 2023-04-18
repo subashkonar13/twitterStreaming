6218f58c2fcf98394b68faa8c054049015a32bcd
subashkonar13 <subashkonar13@gmail.com>
2023-04-19 00:47:05 +0530

    Update collect_tweets_stream.py

--- app/collect_tweets_stream.py
+++ app/collect_tweets_stream.py
@@ -2,7 +2,7 @@ import os
 import json
 import time
 import tenacity
-from tenacity import retry,wait
+from tenacity import *
 import postgres
 import requests
 import pandas as pd
@@ -10,27 +10,44 @@ import tweepy
 import re
 import numpy as np
 import base64
+import nltk
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from typing import List,Dict, Any, Tuple
+import base64
+import requests
 
-with open('credentials.json','r') as f:
-    credential = json.load(f)
+# Define constants
+with open('conf/credentials.json','r') as f:
+    credentials = json.load(f)
 
-CONSUMER_KEY = credential['twitter_api_key']
-CONSUMER_SECRET = credential['twitter_api_secret_key']
-ACCESS_TOKEN = credential['twitter_access_token']
-ACCESS_TOKEN_SECRET = credential['twitter_access_token_secret']
-BEARER_TOKEN = credential['bearer_token']
+CONSUMER_KEY: str = credentials['twitter_api_key']
+CONSUMER_SECRET: str = credentials['twitter_api_secret_key']
+ACCESS_TOKEN: str = credentials['twitter_access_token']
+ACCESS_TOKEN_SECRET: str = credentials['twitter_access_token_secret']
+BEARER_TOKEN: str = credentials['bearer_token']
+QUERY: str = 'IKEA lang:en -is:retweet'
+TWEET_FIELDS: str = "tweet.fields=id,created_at,text,author_id"
+EMOJI_REGEX: re.Pattern = re.compile('[\\u203C-\\u3299\\U0001F000-\\U0001F644]')
+EXPANSIONS: str = "expansions=author_id"
 
-query = 'IKEA lang:en -is:retweet'
-tweet_fields = "tweet.fields=id,created_at,text,author_id"
-emoji = re.compile('[\\u203C-\\u3299\\U0001F000-\\U0001F644]')
-expansions = "expansions=author_id"
-#headers = {"Authorization": "Bearer {}".format(bearer_token)}
-def create_url(*params):
+def create_url(query: str, tweet_fields: str, expansions: str) -> str:
+    """
+    Constructs the search URL for the Twitter API given the query, tweet fields, and expansions.
+
+    Args:
+    - query (str): The search query to use for the API.
+    - tweet_fields (str): The tweet fields to include in the API response.
+    - expansions (str): The expansions to include in the API response.
+
+    Returns:
+    - str: The constructed search URL.
+    """
     url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
         query, tweet_fields, expansions
     )
     return url
+    
+
 
 
 def bearer_oauth(r):
@@ -45,33 +62,39 @@ def bearer_oauth(r):
 
 
 
-#@retry(wait=wait_exponential(multiplier=900, min=2, max=30),  stop=stop_after_attempt(5))
-#@tenacity.retry(wait=tenacity.wait_fixed(10) + wait.wait_random(0, 3))
-def connect_to_endpoint(url):
-    response1 = requests.request("GET", url, auth=bearer_oauth, stream=True)
-    #print(response1.status_code)
+
+@retry(wait=wait_exponential(min=2, max=60), stop=stop_after_attempt(5))
+def connect_to_endpoint(url: str) -> None:
+    """
+    Connects to the given endpoint URL and writes the tweets data to Postgres.
+    """
+    response1 = requests.get(url, auth=bearer_oauth, stream=True)
+    if response1.status_code != 200:
+        """Raise an exception if the response is not 200 (OK)
+        """
+        raise Exception(f"Request returned an error: {response1.status_code} {response1.text}")
     for response_line in response1.iter_lines():
-        if response_line:
-            json_response = json.loads(response_line)
-            #t=json.dumps(json_response, indent=4, sort_keys=True)
-            #columns=['id','created_at','text']
-            for r in json_response['data']:
-                user_id=r['author_id']
-                i=r['id']
-                created_at=r['created_at']
-                text=r['text']
-                rows = [ {"id":i,
-                          "text":text,
-                          "created_at":created_at}]
-                f=list(filter(emoji.match,text))
-                senti=sentimentAnalyse(text)
-                #d=[(i,created_at,text)]
-                #postgres.insert_tweet_data(i,text,created_at)
-                #postgres.writetoPostgres(rows)
-                postgres.writeTweets(user_id,i,text,senti,f,created_at)
-                
-                #d.append([id,created_at,text])
-            #print(d)
+        if not response_line:
+            continue
+        json_response = json.loads(response_line)
+        for tweet in json_response.get('data', []):
+            user_id = tweet['author_id']
+            tweet_id = tweet['id']
+            created_at = tweet['created_at']
+            text = tweet['text']
+            emojis = list(filter(EMOJI_REGEX.match, text))
+            sentiment, comp = sentiment_analyze(text)
+            if sentiment=='neu':
+                    sentiment='Neutral'
+            elif sentiment=='pos':
+                    sentiment='Positive'
+            elif sentiment=='neg':
+                    sentiment='Negative'
+            elif sentiment=='compound':
+                    sentiment='Mixed feelings'
+            else:
+                    sentiment='Not available'
+            postgres.writeTweets(user_id, tweet_id, text, sentiment, comp, emojis, created_at)
 
     if response1.status_code != 200:
         raise Exception(
@@ -79,82 +102,87 @@ def connect_to_endpoint(url):
                 response1.status_code, response1.text
             )
         )
-'''
-Capturing the sentiment having the maximum value 
-'''
+
         
-def sentimentAnalyse(text):
+def sentiment_analyze(text: str) -> Tuple[str, float]:
+    """
+    Analyzes the sentiment of the given text and returns the dominant sentiment and compound score.
+    """
     analyzer = SentimentIntensityAnalyzer()
     vs = analyzer.polarity_scores(text)
-    senti=(max(vs, key=vs.get))
-    return senti
-    
+    sentiment = max(vs, key=vs.get)
+    comp = vs['compound']
+    return sentiment, comp
+
+ 
     
 
 
-def connectUrl():
-    #Reformat the keys and encode them
-    key_secret = '{}:{}'.format(CONSUMER_KEY, CONSUMER_SECRET).encode('ascii')
-    #Transform from bytes to bytes that can be printed
-    b64_encoded_key = base64.b64encode(key_secret)
-    #Transform from bytes back into Unicode
-    b64_encoded_key = b64_encoded_key.decode('ascii')
-    
-    
+
+
+def connectUrl() -> Tuple[Dict[str, Any], Dict[str, str], str]:
+    """Connect to Twitter API and get authorization headers and URL parameters for trends API.
+
+    Returns:
+        A tuple containing the URL parameters, authorization headers, and URL for trends API.
+    """
+    key_secret = f"{CONSUMER_KEY}:{CONSUMER_SECRET}".encode('ascii')
+    b64_encoded_key = base64.b64encode(key_secret).decode('ascii')
     base_url = 'https://api.twitter.com/'
-    auth_url = '{}oauth2/token'.format(base_url)
+    auth_url = f"{base_url}oauth2/token"
     auth_headers = {
-        'Authorization': 'Basic {}'.format(b64_encoded_key),
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        "Authorization": f"Basic {b64_encoded_key}",
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
     }
-    auth_data = {
-        'grant_type': 'client_credentials'
-    }
+    auth_data = {"grant_type": "client_credentials"}
     auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
-    print(auth_resp.status_code)
-    access_token = auth_resp.json()['access_token']
-    trend_headers = {
-        'Authorization': 'Bearer {}'.format(access_token)    
-    }
-    
-    trend_params = {
-        'id': 1,
-    }
-    
-    trend_url = 'https://api.twitter.com/1.1/trends/place.json'  
-    return trend_params,trend_headers,trend_url
+    access_token = auth_resp.json()["access_token"]
+    trend_headers = {"Authorization": f"Bearer {access_token}"}
+    trend_params = {"id": 1}
+    trend_url = f"{base_url}1.1/trends/place.json"
+    return trend_params, trend_headers, trend_url
 
-def extractData():
-    trend_params,trend_headers,trend_url=connectUrl()
-    trend_resp = requests.request("GET",trend_url, headers=trend_headers, params=trend_params,stream=True)
-       #tweet_data = trend_resp.json()
+
+
+def extractData() -> None:
+    """
+    Extracts data from Twitter API and writes it to the database.
+
+    Raises:
+        Exception: If the request returns an error.
+    """
+    trend_params, trend_headers, trend_url = connectUrl()
+    trend_resp = requests.request("GET", trend_url, headers=trend_headers, params=trend_params, stream=True)
+
     for response_line in trend_resp.iter_lines():
-            if response_line:
-               json_response = json.loads(response_line)
-               for r in json_response:
-                  try:
-                     for i in (r['trends']):
-                         trend_name=i['name']
-                         url=i['url']
-                         tweet_volume=i['tweet_volume']
-                         postgres.writeTrends(trend_name,url,tweet_volume)
-                  except TypeError:
-                     print(f"TypeError :{json_response}")
-               
-                    
-                    
+        if response_line:
+            json_response = json.loads(response_line)
+            for r in json_response:
+                try:
+                    for i in r['trends']:
+                        trend_name = i['name']
+                        url = i['url']
+                        tweet_volume = i.get('tweet_volume', None)
+                        postgres.writeTrends(trend_name, url, tweet_volume)
+                except TypeError:
+                    print(f"TypeError: {json_response}")
+
     if trend_resp.status_code != 200:
         raise Exception(
-            "Request returned an error: {} {}".format(
-                trend_resp.status_code, trend_resp.text
-            )
+            f"Request returned an error: {trend_resp.status_code} {trend_resp.text}"
         )
 
 
 
-def main():
-    url = create_url(query, tweet_fields, expansions)
-    timeout = 0
+def main() -> None:
+    """
+    Run the main program loop.
+
+    Returns:
+        None
+    """
+    url: str = create_url(QUERY, TWEET_FIELDS, EXPANSIONS)
+    timeout: int = 0
     while True:
         connect_to_endpoint(url)
         extractData()
